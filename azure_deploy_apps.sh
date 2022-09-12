d4d35727c3602abfb727d10176b07b2cad4a521d
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-09-12 12:26:27 -0700

    updated

--- azure_deploy_apps.sh
+++ azure_deploy_apps.sh
@@ -1,6 +1,11 @@
 file="random.log"
 random=$(cat "$file") 
 
+resourcegroupname=$random-myAuthResourceGroup
+appplanname=$random-plan
+clientappname=$random-client-a
+apiappname=$random-api-b
+
 # Zip client - zip is available in Azure cloud shell
 cd a-client-app-with-user-access-token && zip -r a-client.zip .
 az webapp deploy --resource-group $resourcegroupname --name $clientappname --src-path a-client.zip
--- azure_resource_auth_config.sh
+++
@@ -1,7 +0,0 @@
-file="random.log"
-random=$(cat "$file") 
-
-# Client returns usable access token
-authSettings=$(az webapp auth show -g $resourcegroupname -n <client-app-name>)
-authSettings=$(echo "$authSettings" | jq '.properties' | jq '.identityProviders.azureActiveDirectory.login += {"loginParameters":["scope=openid offline_access api://<api-app-identity-provider-client-id>/user_impersonation"]}')
-az webapp auth set --resource-group myAuthResourceGroup --name <client-app-name> --body "$authSettings"
