312a51e770643f9a31f7850251c307ecfa49fc9c
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-09-12 13:08:10 -0700

    Update 3_azure_deploy_apps.sh

--- 3_azure_deploy_apps.sh
+++ 3_azure_deploy_apps.sh
@@ -8,10 +8,10 @@ apiappname=$random-api-b
 
 # Zip client - zip is available in Azure cloud shell
 cd a-client-app-with-user-access-token && zip -r a-client.zip .
-az webapp deploy --resource-group $resourcegroupname --name $clientappname --src-path a-client.zip
+az webapp deploy --resource-group $resourcegroupname --name $clientappname --type zip --src-path a-client.zip
 cd ..
 
 # Zip API - zip is available in Azure cloud shell
 cd b-api-app-with-user-bearer-token && zip -r b-api.zip .
-az webapp deploy --resource-group $resourcegroupname --name $apiappname --src-path b-api.zip
+az webapp deploy --resource-group $resourcegroupname --name $apiappname --type zip --src-path b-api.zip
 cd ..
